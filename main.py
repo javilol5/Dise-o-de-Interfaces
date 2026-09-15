4cac970f874a20702a65bc55a682cd1977c812f4
casal <javierlolcasal@gmail.com>
2026-09-15 14:40:04 +0200

    ventana con colores

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from PyQt6.QtWidgets import (QMainWindow, QApplication, QVBoxLayout, QPushButton, QWidget, QLabel, QLineEdit, QHBoxLayout)
+from PyQt6.QtGui import QColor, QPalette
 
 class FiestraPrincipal(QMainWindow):
     def __init__(self):
@@ -9,6 +10,13 @@ class FiestraPrincipal(QMainWindow):
         self.setWindowTitle("Primera aplicacion")
         self.setMinimumSize(300, 200)
         self.setMaximumSize(500, 400)
+
+        paleta = self.palette()
+        paleta.setColor(QPalette.ColorRole.Window, QColor("lightblue"))
+        self.setPalette(paleta)
+
+
+
 
         self.show()
 
